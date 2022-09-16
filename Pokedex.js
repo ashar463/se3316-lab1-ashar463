a6f4b78aa22b1f2e2b9e886aff35c093112e1245
ashar463 <ashar463@uwo.ca>
2022-09-16 14:44:51 -0400

    Made the input field and submit button functional. They now present an alert and logic is ready to be coded.

---
+++ Pokedex.js
@@ -0,0 +1,9 @@
+function searchPokemonNum(){
+    var test = document.getElementById("pokeNum").value;
+    alert(test);
+}
+
+function searchPokemonName(){
+    var test = document.getElementById("pokeName").value;
+    alert(test);
+}
